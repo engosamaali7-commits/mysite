53501c007697f9f45c0e44978c309de4ac2a5ad6
OQ | Developer <engosamaali7@gmail.com>
2026-08-01 01:20:53 +0300

    Initial commit : add mysite  project

---
+++ blog/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.urls import reverse
+from django.utils import timezone
+from django.conf import settings
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+
+class Post(models.Model):
+    
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'مسودة'
+        PUBLISHED = 'PB', 'نشرت'
+    
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now) # تاريخ النشر
+    created = models.DateTimeField(auto_now_add=True) # تاريخ الإنشاء
+    updated = models.DateTimeField(auto_now=True) # لحظة آخر تعديل
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='blog_posts')
+    
+    objects = models.Manager() # The default manager.
+    published = PublishedManager() # Our custom manager.
+        
+    class Meta:
+        ordering = ['-publish'] # ترتيب تنازلي
+        indexes = [models.Index(fields=['-publish'])]
+        
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[
+            self.publish.year,
+            self.publish.month,
+            self.publish.day,
+            self.slug
+        ])
